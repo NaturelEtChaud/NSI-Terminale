cb40eb7ab7f479dba9461123f895f28c2bf0c61a
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2021-11-18 12:36:55 +0100

    Add files via upload

---
+++ 3 Récursivité/exercices/ex2_courbe_arbre.py
@@ -0,0 +1,52 @@
+from turtle import *
+shape("turtle")
+speed(0.1)
+
+#longueur du tronc
+l = 200
+#angle de l'inclinaison de la branche
+o = 30
+#rapport entre branche et tronc
+r = 1/2
+#nombre d'appels récursif
+n = 10
+
+
+#la fonction définie par récursivité
+def arbre(n,l,r,o):
+    if n == 0:
+        color('green')
+        forward(l)
+        right(180)
+        forward(l)
+        color('brown')
+    else:
+        #le tronc
+        forward(l)
+        #la branche à droite
+        right(o)
+        arbre(n-1,l*r,r,o)
+        #la branche à gauche
+        right(180-2*o)
+        arbre(n-1,l*r,r,o)
+        #retour à la base du tronc
+        right(o)
+        forward(l)
+
+
+
+#on positionne la tortue
+up()
+right(90)
+forward(l)
+down()
+left(180)
+
+#c'est parti pour faire un beau dessin
+color('brown')
+arbre(n,l,r,o)
+
+
+#pour fermer proprement
+exitonclick()
+mainloop()
