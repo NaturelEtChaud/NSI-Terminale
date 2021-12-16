051ed766b3ed021c56428d3f198820fe2dfba14f
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2021-12-16 10:16:19 +0100

    Update indian_food.sql

--- 4 Base de données/indian_food/indian_food.sql
+++ 4 Base de données/indian_food/indian_food.sql
@@ -1,20 +1,20 @@
 --requetes
 --2) plats venant de l'East
 SELECT * 
-  from indian_food
-  where region = 'East';
+  FROM indian_food
+  WHERE region = 'East';
 
 --3) uniquement le nom et les ingrédients des plats venant de l'East
 SELECT name, ingredients 
-  from indian_food
-  where region = 'East';
+  FROM indian_food
+  WHERE region = 'East';
 
 --4) nombre de plats végétariens
 SELECT COUNT(*)
-  from indian_food
-  where diet = "vegetarian";
+  FROM indian_food
+  WHERE diet = "vegetarian";
 
 --5) nombre de plats végétariens ne contenant pas de lait
 SELECT COUNT(*)
-  from indian_food
-  where (diet = "vegetarian") AND (ingredients NOT LIKE '%Milk%');   
+  FROM indian_food
+  WHERE (diet = "vegetarian") AND (ingredients NOT LIKE '%Milk%');   
