fd579d7ec3303d2f959cee8fb3da650903f09ade
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2022-06-27 21:03:04 +0200

    Update complexites.py

--- 1 Structure de données I/Python/complexites.py
+++ 1 Structure de données I/Python/complexites.py
@@ -20,5 +20,6 @@ plt.plot(liste_n, liste_carre, label='0(n²)')
 axes = plt.gca()
 axes.set_xlim(0, nb) 
 axes.set_ylim(0, nb) 
+plt.xlabel('taille du tableau')
+plt.ylabel("ordre de grandeur du nombre d'opérations")
 plt.legend()
-
